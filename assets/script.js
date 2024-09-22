b4928826976c4c6b02847d11beba38a10a4250bc
lucrece-k <kouamelucrece@gmail.com>
2024-09-22 23:46:59 +0200

    ajout des flèche et des bullet avec js

--- assets/script.js
+++ assets/script.js
@@ -17,15 +17,58 @@ const slides = [
     tagLine: 'Autocollants <span>avec découpe laser sur mesure</span>',
   },
 ];
+// creer la balise img flèche droite
+let flecheRight = document.createElement('img');
+// ajouter les attributs et class de cette balise
+flecheRight.src = './assets/images/arrow_right.png';
+flecheRight.alt = 'la flèche droit';
+flecheRight.classList.add('arrow', 'arrow_right');
+// récupérer l'élément parent dans lequelle la balise sera placer
+let parentFleche = document.getElementById('banner');
+// ajouter la nouvelle balise dans le fichier html
+parentFleche.appendChild(flecheRight);
+
+// meme proseder pour la creation de la flèche gauche
+let flecheLeft = document.createElement('img');
+flecheLeft.src = './assets/images/arrow_left.png';
+flecheLeft.alt = 'la flèche gauche';
+flecheLeft.classList.add('arrow', 'arrow_left');
+parentFleche.appendChild(flecheLeft);
+
+// ajout des bullet points
+let spanBulletUn = document.createElement('span');
+spanBulletUn.classList.add('dot');
+let parentBullet = document.querySelector('.dots');
+parentBullet.appendChild(spanBulletUn);
+
+let spanBulletDeux = document.createElement('span');
+spanBulletDeux.classList.add('dot');
+parentBullet.appendChild(spanBulletDeux);
+
+let spanBulletTrois = document.createElement('span');
+spanBulletTrois.classList.add('dot');
+parentBullet.appendChild(spanBulletTrois);
+
+let spanBulletQuatre = document.createElement('span');
+spanBulletQuatre.classList.add('dot');
+parentBullet.appendChild(spanBulletQuatre);
+
+// recuperer tout les span des bullet
+let spanBullet = document.querySelectorAll('.dots span');
+console.log(spanBullet);
+
 const arrowLeftClick = document.querySelector('.arrow_left');
 console.log(arrowLeftClick);
 arrowLeftClick.addEventListener('click', function (event) {
-  alert('click gauche');
-  console.log(event);
+  spanBullet.classList.add('dot_selected');
+  console.log(spanBullet);
 });
+
 const arrowRightClick = document.querySelector('.arrow_right');
 console.log(arrowRightClick);
-arrowRightClick.addEventListener('click', function (event) {
-  alert('click droit');
-  console.log(event);
+arrowRightClick.addEventListener('click', function () {
+  let i = 0;
+  i++;
+  spanBullet[i].classList.add('dot_selected');
+  console.log(spanBullet[i]);
 });
